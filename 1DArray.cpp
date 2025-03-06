b6089889994c0f40a027fb70576513ecc378161e
makinde46 <makindejeff46@gmail.com>
2025-03-06 23:51:20 +0300

    Added 1DArray.cpp

---
+++ 1DArray.cpp
@@ -0,0 +1,18 @@
+/*
+C ++ Program to show 1D Array
+Author : Gitahi Jeff Makumi
+Reg No : BSCIT-05-0137/2024
+Date : 03/03/2025
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    int j;
+    int marks[10] = {2,4,5,8,8,4};
+    for(j=0;j<10;j++){
+        cout<<marks[j]<<"\t";
+        }
+        return 0;
+        
+    }
